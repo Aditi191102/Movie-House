a8cc0d2c01f8590996d97aeafc71c5ac94d9423a
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2024-12-23 23:25:51 +0530

    Modifications in Sign In and Sign Up form

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,15 @@
-import React from 'react'
-
+import React from "react";
 
 const Header = () => {
   return (
     <div className="absolute px-52 py-2 bg-gradient-to-b from-black z-10">
-
-      <img src="/Screenshot 2024-12-20 234937-Photoroom.png" alt='img' className='w-40'/>
-          
+      <img
+        src="/Screenshot 2024-12-20 234937-Photoroom.png"
+        alt="img"
+        className="w-40"
+      />
     </div>
-  )
-}
+  );
+};
 
 export default Header;
