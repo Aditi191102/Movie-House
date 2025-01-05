00a4af3031df414733cadc04e7cbbdf3bd110472
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2025-01-05 23:50:00 +0530

    Password Visibility

---
+++ src/utils/constants.js
@@ -0,0 +1,15 @@
+export const Movie_House_Logo = "/Screenshot 2024-12-20 234937-Photoroom.png";
+
+export const user_Icon =
+  "https://occ-0-4412-3647.1.nflxso.net/dnm/api/v6/vN7bi_My87NPKvsBoib006Llxzg/AAAABfjwXqIYd3kCEU6KWsiHSHvkft8VhZg0yyD50a_pHXku4dz9VgxWwfA2ontwogStpj1NE9NJMt7sCpSKFEY2zmgqqQfcw1FMWwB9.png?r=229";
+
+export const background_Img =
+  "https://assets.nflxext.com/ffe/siteui/vlv3/150c4b42-11f6-4576-a00f-c631308b1e43/web/IN-en-20241216-TRIFECTA-perspective_915a9055-68ad-4e81-b19a-442f1cd134dc_large.jpg";
+
+  export const API_OPTIONS = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3ZjUwYTU5N2JmMjgyMmE4MzcxMTU0N2U1NzRkMWM5MiIsIm5iZiI6MTczNTk3OTQ2MS43MTUwMDAyLCJzdWIiOiI2Nzc4ZjFjNWFiNWVjNGMzZGM3MjdhODMiLCJzY29wZXMiOlsiYXBpX3JlYWQiXSwidmVyc2lvbiI6MX0.7PWWICBhemQ8LplqNSuNzWpmu_4XqzPDzuSqsUb8NiU'
+    }
+  };
