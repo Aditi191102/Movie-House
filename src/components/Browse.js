d59df18c5cff13b7f6ff2f93edb561bb3c9e08e6
Aditi191102 <105860777+Aditi191102@users.noreply.github.com>
2025-01-24 22:55:32 +0530

    Remove hosting.YnVpbGQ.cache from version control

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -11,7 +11,6 @@ const Browse = () => {
   const nowPlayingMovies = async () => {
     const data = await fetch('https://api.themoviedb.org/3/movie/now_playing?page=1', API_OPTIONS)
     const movieData = await data.json();
-   
     dispatch(addNowPlayingMovies(movieData.results));
   }
 
